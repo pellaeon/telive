5195a2dc5cfce8341211dc667c116fb582107596
Pellaeon Lin <nfsmwlin@gmail.com>
2026-01-02 14:03:18 +0800

    FIX: grc path

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gnuradio-companion /app/gnuradio-companion/receiver_udp/telive_1ch_simple_gr37_udp.grc &
+gnuradio-companion /app/gnuradio-companion/python2_based_gnuradio/receiver_udp/telive_1ch_simple_gr37_udp.grc &
 xterm -hold -e "cd ~/tetra/osmo-tetra-sq5bpf/src/ && ./receiver1udp 1" &
 xterm -hold -geom 203x60 -e 'export PATH="/tetra/bin:/app/bin:$PATH" && cd /app && ./rxx' &
 /bin/bash
